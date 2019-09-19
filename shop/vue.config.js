959994f51fac74cb86bb53930eabe6a79cc393f4
Diaz Muñoz <manuelalejandro.diaz@libertyseguros.es>
2019-09-19 17:09:11 +0200

    fix: configutations

--- shop/vue.config.js
+++ shop/vue.config.js
@@ -1,4 +1,12 @@
 // vue.config.js
 module.exports = {
-    assetsDir: 'src/assets',
-  }
+  assetsDir: 'src/assets',
+  // Other options...
+  pluginOptions: {
+    // Express-related options
+    express: {
+        shouldServeApp: true,
+        serverDir: './srv',
+    },
+  },
+}
