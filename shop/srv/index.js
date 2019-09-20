fc416583a3ce82d566675eb0a9f64a741840bf5b
Diaz Muñoz <alexvkcr@gmail.com>
2019-09-20 14:50:38 +0200

    feature: endpoint /items

--- shop/srv/index.js
+++ shop/srv/index.js
@@ -1,24 +1,28 @@
 import express from 'express';
-// import socketIO from "socket.io";
 
 export default (app, http) => {
  app.use(express.json());
  
+ //GET
  app.get('/foo', (req, res) => {
    res.json({msg: 'foo'});
  });
+
  
+ app.get('/items', (req, res) => {
+  res.json({msg: getItems()});
+});
+
+ //POST
  app.post('/bar', (req, res) => {
    res.json(req.body);
  });
- 
-  // optional support for socket.io
-  // 
-  // let io = socketIO(http);
-  // io.on("connection", client => {
-  //   client.on("message", function(data) {
-  //     // do something
-  //   });
-  //   client.emit("message", "Welcome");
-  // });
+}
+
+function getItems(){
+  let item = {id:0, name: '', description:'', price:''} 
+  let items =[]
+  items.push(Object.assign({},item,{id:1, name: 'Bollito', description:'de Crema', price:30}))
+  items.push(Object.assign({},item,{id:2, name: 'Palmera', description:'de chocolate', price:30}))
+  return items
 }
