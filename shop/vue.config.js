9a97e2ce857da14ab3315bf28179adabf61c344c
Diaz Muñoz <alexvkcr@gmail.com>
2019-09-20 13:31:34 +0200

    fix: express installed and configured

--- shop/vue.config.js
+++ shop/vue.config.js
@@ -3,10 +3,9 @@ module.exports = {
   assetsDir: 'src/assets',
   // Other options...
   pluginOptions: {
-    // Express-related options
     express: {
-        shouldServeApp: true,
-        serverDir: './srv',
+      shouldServeApp: true,
+      serverDir: './srv'
     },
   },
 }
