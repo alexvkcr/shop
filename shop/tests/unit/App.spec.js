43c74f4cf102ff1f442e86af0d228da92c305600
Diaz Muñoz <alexvkcr@gmail.com>
2019-10-17 09:20:37 +0200

    fix: last warning in tests fixed

--- shop/tests/unit/App.spec.js
+++ shop/tests/unit/App.spec.js
@@ -1,10 +1,21 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import App from '@/App.vue'
+import AppHeader from '../../src/components/AppHeader.vue'
+import SideBar from '../../src/components/SideBar.vue'
+import AppFooter from '../../src/components/AppFooter.vue'
+
+
+const localVue = createLocalVue()
+localVue.use(AppHeader)
+localVue.use(SideBar)
+localVue.use(AppFooter)
+
+
 
 describe('App.vue', () => {
   it('has the proper id', () => {
     const id = 'app'
-    const wrapper = shallowMount(App)
+    const wrapper = shallowMount(App, localVue)
     expect(wrapper.attributes().id).toContain(id)
   })
 })
