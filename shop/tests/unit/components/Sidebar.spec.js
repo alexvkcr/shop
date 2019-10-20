22cce985b4b6bfa16de2a474d3b93077aadb150e
Alejandro Díaz <alexvkcr@gmail.com>
2019-10-20 22:03:49 +0200

    test: fixing SideBar test, a tough way to go

--- shop/tests/unit/components/Sidebar.spec.js
+++ shop/tests/unit/components/Sidebar.spec.js
@@ -1,25 +1,29 @@
 import { shallowMount } from '@vue/test-utils'
 import VueRouter from 'vue-router'
 import SideBar from '@/components/SideBar.vue'
+import Buefy from 'buefy'
+
+
+const routes = [
+  { path: '/' },
+  { path: '/items' },
+  { path: '/headquarters' }
+]
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes
+})
+const localVue = createLocalVue()
+localVue.use(router)
+localVue.use(Buefy)
 
 
 describe('SideBar.vue', () => {
   it('has the proper id', () => {
     const id = 'SideBar'
-    const routes = [
-      { path: '/' },
-      { path: '/items' },
-      { path: '/headquarters' }
-    ]
-    const router = new VueRouter({
-      mode: 'history',
-      base: __dirname,
-      routes
-    })
-    const wrapper = shallowMount(SideBar, {
-      stubs: ['router-link', 'router-view'],
-      name: 'SideBar',
-      router
+    const wrapper = shallowMount(SideBar,{localVue, 
+      name: 'SideBar'
     })
     expect(wrapper.attributes().id).toContain(id)
   })
