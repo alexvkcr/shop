7b073285a1de11627a5ff269d2bd40dd2eb25cd6
Diaz Muñoz <alexvkcr@gmail.com>
2019-10-29 10:14:01 +0100

    fet/more items to display

--- shop/srv/index.js
+++ shop/srv/index.js
@@ -25,6 +25,22 @@ function getItems() {
     let item = { id: 0, name: '', description: '', allergens: [], price: '' }
     let items = []
     items.push(Object.assign({}, item, { id: 1, name: 'Bollito', description: 'de Crema', allergens: ['Gluten'], price: 30 }))
-    items.push(Object.assign({}, item, { id: 2, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 2, name: 'Palmera', description: 'de Chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 3, name: 'Palmera', description: 'de Nata', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 4, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 5, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 6, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 7, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 8, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 9, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 10, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 11, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 12, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 13, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 14, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 15, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 16, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 17, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
+    items.push(Object.assign({}, item, { id: 18, name: 'Palmera', description: 'de chocolate', allergens: ['Gluten'], price: 30 }))
     return items
 }
