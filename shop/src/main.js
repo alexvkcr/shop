a7ec3e1c482116f0512a22feafd384489c0ad890
Diaz Muñoz <alexvkcr@gmail.com>
2019-10-30 12:43:42 +0100

    feat/added vuex

--- shop/src/main.js
+++ shop/src/main.js
@@ -1,5 +1,5 @@
-
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import Buefy from 'buefy';
@@ -13,35 +13,43 @@ import Headquarters from './components/Headquarters.vue'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(Vuex)
 Vue.use(Buefy)
 Vue.use(VueGoogleMaps, {
-  load: {
-    key: '',
-    libraries: 'places', // This is required if you use the Autocomplete plugin
-    // OR: libraries: 'places,drawing'
-    // OR: libraries: 'places,drawing,visualization'
-    // (as you require)
- 
-    //// If you want to set the version, you can do so:
-    // v: '3.26',
-  }
+    load: {
+        key: '',
+        libraries: 'places', // This is required if you use the Autocomplete plugin
+        // OR: libraries: 'places,drawing'
+        // OR: libraries: 'places,drawing,visualization'
+        // (as you require)
+
+        //// If you want to set the version, you can do so:
+        // v: '3.26',
+    }
+})
+
+const store = new Vuex.Store({
+    state: {
+        user: ""
+    }
 })
 
 const routes = [
-  { name: 'index', path: '/', component: Index },
-  { name: 'items', path: '/items', component: Items },
-  { name: 'headquarters', path: '/headquarters', component: Headquarters }
+    { name: 'index', path: '/', component: Index },
+    { name: 'items', path: '/items', component: Items },
+    { name: 'headquarters', path: '/headquarters', component: Headquarters }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: __dirname,
-  routes
+    mode: 'history',
+    base: __dirname,
+    routes
 })
 
 new Vue({
-  el: '#app', 
-  components: { App },
-  router,
-  render: h => h(App),
+    el: '#app',
+    components: { App },
+    router,
+    store,
+    render: h => h(App),
 }).$mount('#app')
