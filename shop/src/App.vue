e8831c76e39ff929199b02697e19c68af5d90363
Diaz Muñoz <alexvkcr@gmail.com>
2019-11-05 15:34:13 +0100

    feat/custom scrollbar

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -69,4 +69,23 @@ export default {
       "footer footer";
   }
 }
+/*Section for the custom scrollbar*/
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
 </style>
