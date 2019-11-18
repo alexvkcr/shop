5ad85c0422f426e9a9aed0a0cd09461e19ddf4cd
Diaz Muñoz <alexvkcr@gmail.com>
2019-11-18 17:16:05 +0100

    feat/insertion of img in the Index

--- shop/src/components/Index.vue
+++ shop/src/components/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="Index">
-    Sección de presentación
+    <img class="img" :src="require(`@/assets/img/pretzel.jpg`)" alt="Imagen de Pretzel" >
   </div>
 </template>
 
@@ -12,6 +12,19 @@ export default {
 </script>
 
 <style>
-#index {
+#Index {  
+}
+.img{
+  display: block;
+  width: 80vw;
+  margin: auto;
+  padding: 0 3em;
+  transform: scale(1.2,1.2);
+  transition: transform 3s cubic-bezier(.19,1.95,.43,.48)
+}
+@media screen and (min-width: 490px) {
+  .img{
+    width: 60vw;
+  }
 }
 </style>
