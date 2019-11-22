2fca59aa8467ea94b4e81841fc6031b1bd86b3ec
Diaz Muñoz <alexvkcr@gmail.com>
2019-11-22 13:27:36 +0100

    feat/init new page

---
+++ shop/src/components/ItemPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="ItemPage" :item-page-id='thing.id'>
+    <p class="">HOLA{{ thing.name }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ItemPage',
+  props: {
+    thing: {
+        id: String,
+        name: String,
+        description: String,
+        allergens: {
+          type: Array,
+          required: true,
+          default: [],
+          validator: allergens => (!Array.isArray(allergens)) ? false : ! allergens.some(it => typeof(it)!='string')
+        },
+        price: Number,
+        img: String
+        }
+    },
+    data:()=> {
+      return {
+      }
+    },
+    methods:{
+    }
+}
+</script>
+
+<style>
+#ItemPage {
+  
+}
+</style>
