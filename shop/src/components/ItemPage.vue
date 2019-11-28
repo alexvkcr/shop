d5ebf888b88179d0bd17d6efeb1f4e511ec02af5
Diaz Muñoz <alexvkcr@gmail.com>
2019-11-28 10:12:21 +0100

    feat/img displayed in ItemPage

--- shop/src/components/ItemPage.vue
+++ shop/src/components/ItemPage.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="ItemPage" >
+  <div id="ItemPage">
     <h2 class="titleItem">{{thing.name}}</h2>
+    <img class="imgLarge" :src="require(`@/assets/${thing.img}`)" :alt="`Imagen de ${thing.name}`" >
   </div>
 </template>
 
@@ -47,10 +48,24 @@ export default {
 #ItemPage {
   
 }
+.imgLarge{
+  width: 100%;
+  max-width: 400px;
+  margin: 10px auto;
+}
 .titleItem{
   font-weight: bold;
   font-size: 20px;
   margin: 10px auto;
   margin-left: 45%
+}
+
+@media screen and (min-width: 490px) { 
+  #ItemPage{
+    padding-left: calc(50% - 240px)
+  }
+  .titleItem{
+    margin: 10px 120px;
+  }
 }
 </style>
