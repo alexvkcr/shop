693bb96f5c75c4815351205db3b10bcc2be46289
Diaz Muñoz <manuelalejandro.diaz@libertyseguros.es>
2019-12-12 14:48:37 +0100

    ongoing feat/Machine XState for Login

--- shop/src/components/AppHeader.vue
+++ shop/src/components/AppHeader.vue
@@ -55,7 +55,120 @@ const ModalForm = {
                     </footer>
                 </div>
             </form>
-        `
+        `,
+        created(){
+          const machineConfig = Machine({
+            id: 'signIn',
+            context: {
+              email: '',
+              password: ''
+            },
+            initial: 'dataEntry',
+            states: {
+              dataEntry: {
+                on: {
+                  ENTER_EMAIL: {
+                    actions: 'cacheEmail'
+                  },
+                  ENTER_PASSWORD: {
+                    actions: 'cachePassword'
+                  },
+                  EMAIL_BLUR: {
+                    cond: 'isBadEmailFormat',
+                    target: 'emailErr.badFormat'
+                  },
+                  PASSWORD_BLUR: {
+                    cond: 'isPasswordShort',
+                    target: 'passwordErr.tooShort'
+                  },
+                  SUBMIT: [
+                    {
+                      cond: 'isBadEmailFormat',
+                      target: 'emailErr.badFormat'
+                    },
+                    {
+                      cond: 'isPasswordShort',
+                      target: 'passwordErr.tooShort'
+                    },
+                    {
+                      target: 'awaitingResponse'
+                    }
+                  ]
+                }
+              },
+              awaitingResponse: {
+                invoke: {
+                  src: 'requestSignIn',
+                  onDone: {
+                    target: 'signedIn'
+                  },
+                  onError: [
+                    {
+                      cond: 'isNoAccount',
+                      target: 'emailErr.noAccount'
+                    },
+                    {
+                      cond: 'isIncorrectPassword',
+                      target: 'passwordErr.incorrect'
+                    },
+                    {
+                      cond: 'isServiceErr',
+                      target: 'serviceErr'
+                    }
+                  ]
+                }
+              },
+              emailErr: {
+                onEntry: 'focusEmailInput',
+                on: {
+                  ENTER_EMAIL: {
+                    // target: 'dataEntry',
+                    actions: 'cacheEmail'
+                  }
+                },
+                initial: 'badFormat',
+                states: {
+                  badFormat: {},
+                  noAccount: {}
+                }
+              },
+              passwordErr: {
+                onEntry: 'focusPasswordInput',
+                on: {
+                  ENTER_PASSWORD: {
+                    target: 'dataEntry',
+                    actions: 'cachePassword'
+                  }
+                },
+                initial: 'tooShort',
+                states: {
+                  tooShort: {},
+                  incorrect: {}
+                }
+              },
+              serviceErr: {
+                onEntry: 'focusSubmitBtn',
+                on: {
+                  SUBMIT: {
+                    target: 'awaitingResponse'
+                  }
+                }
+              },
+              signedIn: {
+                type: 'final'
+              }
+            },
+            onDone: {
+              actions: 'onAuthentication'
+            }
+          })
+      },
+      methods:{
+        cancelLog(){
+          this.$parent.$parent.$emit('CANCEL')
+          this.$parent.close();
+        }
+      }
     }
 
 export default {
