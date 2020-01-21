e7ef3ab745eaa9fb90bf3f733a8795724b2af9a4
Diaz Muñoz <alexvkcr@gmail.com>
2020-01-21 14:03:40 +0100

    fix:  user log restablished

--- shop/src/components/AppHeader.vue
+++ shop/src/components/AppHeader.vue
@@ -19,6 +19,47 @@
 
 <script>
 
+
+const ModalForm = {
+        props: ['email', 'password'],
+        template: `
+            <form action="">
+                <div class="modal-card" style="width: auto">
+                    <header class="modal-card-head">
+                        <p class="modal-card-title">Login</p>
+                    </header>
+                    <section class="modal-card-body">
+                        <b-field label="Email">
+                            <b-input
+                                type="email"
+                                :value="email"
+                                placeholder="Your email"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                        <b-field label="Password">
+                            <b-input
+                                type="password"
+                                :value="password"
+                                password-reveal
+                                placeholder="Your password"
+                                required>
+                            </b-input>
+                        </b-field>
+
+                        <b-checkbox>Remember me</b-checkbox>
+                    </section>
+                    <footer class="modal-card-foot">
+                        <button class="button" type="button" @click="cancelLog()">Cerrar</button>
+                        <button class="button is-primary">Login</button>
+                    </footer>
+                </div>
+            </form>
+        `
+    }
+
+
 export default {
   name: 'AppHeader',
   methods:{
@@ -29,7 +70,6 @@ export default {
             hasModalCard: true,
             customClass: 'custom-class custom-class-2'
         })
-        this.logUserState()
     },
     showSideBar(){
       let sideBar = document.querySelector('#SideBar')
